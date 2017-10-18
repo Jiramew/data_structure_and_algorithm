418395fbb5c9cf55ddc624ad039b87691d86c433
yanghanbing <yanghb@lhcis.com>
2017-10-18 17:38:35 +0800

    Add: quick3way

--- algorithm/src/main/java/sort/Quick.java
+++ algorithm/src/main/java/sort/Quick.java
@@ -6,7 +6,30 @@ package sort;
 public class Quick extends SortTemplate {
     public static void sort(Comparable[] a) {
         int n = a.length;
-        sort(a, 0, n - 1);
+        //sort(a, 0, n - 1);
+        sort3way(a, 0, n - 1);
+    }
+
+    public static void sort3way(Comparable[] a, int lo, int hi) {
+        if (hi <= lo) {
+            return;
+        }
+        int lt = lo;
+        int i = lo + 1;
+        int gt = hi;
+        Comparable v = a[lo];
+        while (i <= gt) {
+            int cmp = a[i].compareTo(v);
+            if (cmp < 0) {
+                exch(a, lt++, i++);
+            } else if (cmp > 0) {
+                exch(a, i, gt--);
+            } else {
+                i++;
+            }
+            sort(a, lo, lt - 1);
+            sort(a, gt + 1, hi);
+        }
     }
 
     public static void sort(Comparable[] a, int lo, int hi) {
